a56247218e81215fd05f2c7b37a17bb8bc7e88e6
JohnRuanee <johnruanee@gmail.com>
2023-02-04 16:00:34 -0500

    profile card changes

--- frontend/dshare/src/components/Profiles.js
+++ frontend/dshare/src/components/Profiles.js
@@ -64,26 +64,10 @@ function Profile(username) {
             <div className="flex-grow-1 ms-3">
             <MDBCardTitle>data.name</MDBCardTitle>
             <MDBCardText>data.type</MDBCardText>
+            <MDBCardText>data.location</MDBCardText>
+            <MDBCardText>data.vehicle</MDBCardText>
 
-            <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
-                style={{ backgroundColor: '#efefef' }}>
-                <div>
-                <p className="small text-muted mb-1">Articles</p>
-                <p className="mb-0">41</p>
-                </div>
-                <div className="px-3">
-                <p className="small text-muted mb-1">Followers</p>
-                <p className="mb-0">976</p>
-                </div>
-                <div>
-                <p className="small text-muted mb-1">Rating</p>
-                <p className="mb-0">8.5</p>
-                </div>
-            </div>
-            <div className="d-flex pt-1">
-                <MDBBtn outline className="me-1 flex-grow-1">Chat</MDBBtn>
-                <MDBBtn className="flex-grow-1">Follow</MDBBtn>
-            </div>
+            
             </div>
         </div>
         </MDBCardBody>
